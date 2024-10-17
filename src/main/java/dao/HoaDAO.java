e4be13684cd847465fecfcbf050cc497785864f2
ntthanh1976 <ntthanh01011976@gmail.com>
2024-10-17 13:02:11 +0700

    given

--- src/main/java/dao/HoaDAO.java
+++ src/main/java/dao/HoaDAO.java
@@ -2,13 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model;
+package dao;
 
 import context.DbContext;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import model.Hoa;
 
 /**
  *
